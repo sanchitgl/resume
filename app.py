9f48a2bfa45c1a297b070d9f9225a5055dec30a8
sanchitgoel7 <105559691+sanchitgoel7@users.noreply.github.com>
2022-09-30 01:33:51 +0530

    app changes

---
+++ app.py
@@ -0,0 +1,211 @@
+import streamlit as st
+from PIL import Image
+
+with open("style.css") as f:
+    st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+#####################
+# Header 
+st.write('''
+# Hi! I'm Sanchit Goel 
+''')
+im, desc = st.columns([1,3])
+with im:
+    image = Image.open('media/dp.jpg')
+    st.image(image, width=150)
+with desc:
+    st.write("I am currently a data scientist at Fractal.ai, and interested in research relating to applications of NLP and AI/ML in the health care and social sciences space.")
+
+#####################
+# Navigation
+
+st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+
+st.markdown("""
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #16A2CB;">
+  <a class="navbar-brand" href="https://www.linkedin.com/in/-sanchitgoel" target="_blank">Sanchit Goel</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link disabled" href="/">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#work-experience">Work Experience</a>
+      </li>
+        <li class="nav-item">
+        <a class="nav-link" href="#research">Research</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#past-projects">Projects</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#skills">Skills</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+""", unsafe_allow_html=True)
+
+#####################
+# Custom function for printing text
+def txt5(a):
+    st.markdown(a)
+
+
+def txt(a, b):
+  col1, col2 = st.columns([4,1])
+  with col1:
+    st.markdown(a)
+  with col2:
+    st.markdown(b)
+
+def txt2(a, b):
+  col1, col2 = st.columns([1,4])
+  with col1:
+    st.markdown(f'`{a}`')
+  with col2:
+    st.markdown(b)
+
+def txt3(a, b):
+  col1, col2 = st.columns([1,2])
+  with col1:
+    st.markdown(a)
+  with col2:
+    st.markdown(b)
+  
+def txt4(a, b, c):
+  col1, col2, col3 = st.columns([1.4,4,0.25])
+  with col1:
+    st.markdown(a)
+  with col2:
+    st.markdown(b)
+  with col3:
+    st.markdown(c)
+
+################
+st.markdown('''
+## Work Experience
+''')
+
+txt('**Data Scientist**,&nbsp&nbsp[**Fractal.ai**](https://fractal.ai/)',
+'Oct 2020-Present')
+st.markdown('''
+  -Developed an intelligent automation tool that automates the processing of insurance claims (PA and FWA) for US health insurance firms. Solution digitizes and extracts relevant details from the insurance forms using Natural Language Processing (NLP) models/libraries like Spacy, PYSBD, and Regex, and applies validation rules to check if the claim is valid.
+  \n-Worked on developing an automated Invoice processing solution using Image OCR and Natural lan- guage processing. Trained a text classifier to identify relevant segments of an invoice. Used NLP to extract required information using pattern matching.
+  ''')
+
+#####################
+# st.markdown('''
+# ## Education
+# ''')
+
+# txt('**Post Graduate Diploma (Computer Science)**,&nbsp *Ashoka University*, India',
+# '2019-2020')
+# st.markdown('''
+#   &nbspMinors in Computer Science, GPA: `3.6`, &nbsp Dean's List
+#   ''', unsafe_allow_html = True)
+
+# txt('**Bachelors of Arts (Economics)**,&nbsp *Ashoka University*, India',
+# '2016-2019')
+# st.markdown('''
+# &nbspMajors in Economics, GPA: `3.4`
+# ''')
+# st.write("##")
+#####################
+st.markdown('''
+## Research
+''')
+
+# txt('**NLP Engineer**, Fractal Analytics',
+# 'Oct 2020-Present')
+# st.markdown('''
+#   -Developed an intelligent automation tool that automates the processing of insurance claims (PA and FWA) for US health insurance firms. Solution digitizes and extracts relevant details from the insurance forms using Natural Language Processing (NLP) models/libraries like Spacy, PYSBD, and Regex, and applies validation rules to check if the claim is valid.
+#   \n-Worked on developing an automated Invoice processing solution using Image OCR and Natural lan- guage processing. Trained a text classifier to identify relevant segments of an invoice. Used NLP to extract required information using pattern matching.
+#   ''')
+
+#st.write("##")
+txt5('** Decentralization and Program Implementation**  - Research Assistant, &nbsp [Dr. Ashwini Deshpande](https://scholar.google.com/citations?user=gH37QQkAAAAJ&hl=en)')
+st.markdown('''
+  Worked as a research assistant on the working paper ‘Decentralization and Program Implementation’ to study the impact of district splits on the effectiveness of Program implementation. I prepared the dataset of over 800 districts and 7000 blocks using web scraping and census datasets. I identified district splits in India from 2011-20 by mapping block level changes in a district year over year, allowing us to study how district splits affected NREGA implementation in India from 2010-20.
+  ''')
+
+st.markdown("""---""")
+txt5('[**Words matter: Gender, Jobs and Applicant Behaviour \[IZA\]**](https://docs.iza.org/dp14497.pdf) - Research Assistant, &nbsp [Dr. Kanika Mahajan](https://sites.google.com/view/kanikamahajan/home?authuser=0)')
+st.markdown('''
+  Worked as a research assistant on the paper ‘Words matter: Gender, Jobs and Applicant Behaviour’, to examine how implicit gender preference in Job ads leads to differential labor market out- comes. I used NLP techniques like tf-idf, topic modeling and word clouds to understand the difference between the kind of jobs offered and the keywords associated with respective gender preferred jobs.
+  ''')
+
+st.markdown("""---""")
+txt5('**[Covid 19 and Supply chain disruption \[AAEA\]](https://onlinelibrary.wiley.com/doi/full/10.1111/ajae.12158)** - Research Assistant, &nbsp [Dr. Kanika Mahajan](https://sites.google.com/view/kanikamahajan/home?authuser=0)')
+st.markdown('''
+  Worked on the now published journal ‘Covid 19 and Supply chain disruption’. I was responsible for cleaning the data and calculating the distances between production zones and retail centres by using google maps API to find the impact of distance on supply chain disruptions.
+  ''')
+
+st.markdown("""---""")
+txt5('**[Auto Mobile Slowdown: A Qunatitative Study](https://drive.google.com/file/d/1hq8DhX0UYkx2-82XsmB1sxAa39emTvap/view?usp=sharing)** - Indpendent Study Module, [Dr. Debayan Gupta](https://debayangupta.com/work.htm)')
+st.markdown('''
+  Studied the role factors like regulation changes, NBFC crisis, rise in ride hailing services and consumer confidence played on bringing about the Automobile slowdown in 2019. Used OLS estimation to find the impact of mentioned factors.
+  ''')
+
+
+# st.write("##")
+# txt('**Business Analyst Intern**, National Skill Foundation of India',
+# 'June - July 2017')
+# st.markdown('''
+#   Went to Jharkhand to study the Lac industry. Surveyed over 30 farmers and created a micro business plan for improving current NSFI’s college curriculum on growing LAC.
+#   ''')
+
+#####################
+
+twitterazi_file = open('media/Twitterazi_demo_2.mp4', 'rb')
+twitterazi_bytes = twitterazi_file.read()
+
+stocksnapshot_file = open('media/Stock_snapshot_demo.mp4', 'rb')
+stocksnapshot_bytes = stocksnapshot_file.read()
+
+bookspine_file = open('media/Book_spine_demo.mp4', 'rb')
+bookspine_bytes = bookspine_file.read()
+#####################
+st.write("&nbsp;")
+st.markdown('''
+## Past Projects
+''')
+txt4('**Book Spine Detection**', 'Developed an app for a client that segregates and digitizes the books in a book shelf\'s image. Uses canny edge detection method to detect edges in a photo and then applies hough transformation to identify book spine edges. Then the image is cropped on the identified edges and digitized to get the text on spine.', 'none')
+space, exp, space = st.columns([1.4,4,0.25])
+with exp:
+  with st.expander("See Demo"):
+    st.video(bookspine_bytes)
+st.markdown("""---""")
+txt4('**Twitterazi**', 'A web app that finds the topics a twitter user has tweeted about in the past month. It accurately recognises people, organisations, locations and keywords in the tweets using Spacy and Regex, which can then be used to filter out tweets containing that entity.', '[Link](https://sanchitgoel7-twitterazi-app-ntuijm.streamlitapp.com/)')
+space, exp, space = st.columns([1.4,4,0.25])
+with exp:
+  with st.expander("See Demo"):
+    st.video(twitterazi_bytes)
+st.markdown("""---""")
+
+txt4('**Reconstruction of Obfuscated Images**', 'Used Convolutional Autoencoders to fix 3 types of Obfuscations(blur, pixelation and speckle noise) in facial images. We trained 3 different Autencoders on about 13000 images to handle the different types of obfuscations', '[PDF](https://drive.google.com/file/d/1ac9nbv1E7MKduiapijJC-A8mCk1n9XAZ/view?usp=sharing)')
+st.markdown("""---""")
+txt4('**Hand Gesture Recognition**', 'Trained a convolutional neural network(CNN) to recognize hand gestures. We used VPLU dataset of 1100 images to train the classifier, the model was able to correctly classify hand gestures into 11 classes with ∼ 98% accuracy.', '[PDF](https://drive.google.com/file/d/1tb3uY4i6B9PloM7Pl08O8VBR4N9WbS5E/view?usp=sharing)')
+st.markdown("""---""")
+txt4('**Stocksnapshot**', 'Developed a one-stop stock analysis tool that gives a financial overview of a company by scraping over 20yr+ financial data. The App also has a built in DCF calculator for a quick Intrinsic value calculation of a stock.', '[Link](https://stocksnapshot.herokuapp.com/) &nbsp &nbsp [Github](https://stocksnapshot.herokuapp.com/)')
+space, vid, space = st.columns([1.4,4,0.25])
+with vid:
+  with st.expander("See Demo"):
+    st.video(stocksnapshot_bytes)
+
+
+#####################
+st.markdown('''
+## Skills
+''')
+txt3('Programming', '`Python`, `SQL`,`Java`')
+txt3('Natural Language Processing','`Spacy`,`NLTK`,`PYSBD`,`TF-DF`,`LDA`')
+txt3('Data visualization', '`PowerBi`,`Tableau`,`matplotlib`, `seaborn`, `altair`')
+txt3('Machine Learning', '`scikit-learn`,`TensorFlow`')
+txt3('User Interface', '`streamlit`, `CSS`')
+txt3('Model deployment', '`streamlit`,`Heroku`, `AWS`, `Azure`')
+
+
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 from streamlit_timeline import timeline
-st.set_page_config(page_title="Timeline Example", layout="wide")
+#st.set_page_config(page_title="Timeline Example", layout="wide")
 with open("style.css") as f:
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 
@@ -18,24 +18,15 @@ with open("style.css") as f:
 # image = Image.open('dp.png')
 # st.image(image, width=150)
 
-st.markdown('## My Data science journey', unsafe_allow_html=True)
-# load data
-
-
-with open('example.json', "r") as f:
-    data = f.read()
-
-# render timeline
-timeline(data, height=500)
 # st.markdown('''
 # <iframe src="https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1xuY4upIooEeszZ_lCmeNx24eSFWe0rHe9ZdqH2xqVNk&font=Default&lang=en&initial_zoom=2&height=100%" width="100%" frameborder="0"></iframe>''', unsafe_allow_html = True)
                                 
-st.markdown('## Summary', unsafe_allow_html=True)
-st.info('''
-- Experienced Educator, Researcher and Administrator with almost twenty years of experience in data-oriented environment and a passion for delivering insights based on predictive modeling. 
-- Strong verbal and written communication skills as demonstrated by extensive participation as invited speaker at `10` conferences as well as publishing 149 research articles.
-- Strong track record in scholarly research with H-index of `32` and total citation of 3200+.
-''')
+# st.markdown('## Summary', unsafe_allow_html=True)
+# st.info('''
+# - Experienced Educator, Researcher and Administrator with almost twenty years of experience in data-oriented environment and a passion for delivering insights based on predictive modeling. 
+# - Strong verbal and written communication skills as demonstrated by extensive participation as invited speaker at `10` conferences as well as publishing 149 research articles.
+# - Strong track record in scholarly research with H-index of `32` and total citation of 3200+.
+# ''')
 
 #####################
 # Navigation
@@ -60,7 +51,7 @@ st.markdown("""
         <a class="nav-link" href="#work-experience">Work Experience</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#bioinformatics-tools">Bioinformatics Tools</a>
+        <a class="nav-link" href="#projects">Projects</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#social-media">Social Media</a>
@@ -70,156 +61,168 @@ st.markdown("""
 </nav>
 """, unsafe_allow_html=True)
 
+# space, time, space = st.columns([0.2,8,0.2])
+# with time:
+#   st.markdown('## &nbsp &nbsp &nbsp &nbsp  My Data science journey', unsafe_allow_html=True)
+#   # load data
+
+
+#   with open('example.json', "r") as f:
+#       data = f.read()
+
+#   # render timeline
+#   timeline(data, height=500)
 #####################
 # Custom function for printing text
 def txt(a, b):
-  col1, col2 = st.columns([4,1])
+  space, col1, col2 = st.columns([0.1,4,1])
   with col1:
     st.markdown(a)
   with col2:
     st.markdown(b)
 
 def txt2(a, b):
-  col1, col2 = st.columns([1,4])
+  space, col1, col2 = st.columns([0.5,1,4])
   with col1:
     st.markdown(f'`{a}`')
   with col2:
     st.markdown(b)
 
 def txt3(a, b):
-  col1, col2 = st.columns([1,2])
+  space, col1, col2 = st.columns([0.5,1,2])
   with col1:
     st.markdown(a)
   with col2:
     st.markdown(b)
   
 def txt4(a, b, c):
-  col1, col2, col3 = st.columns([1.5,2,2])
+  space, col1, col2, col3 = st.columns([0.15,0.8,3.5,1])
   with col1:
-    st.markdown(f'`{a}`')
+    st.markdown(a)
   with col2:
     st.markdown(b)
   with col3:
     st.markdown(c)
 
 #####################
+
 st.markdown('''
 ## Education
 ''')
 
-txt('**Post Graduate Diploma** (Computer Science), *Ashoka University*, India',
+txt('**Post Graduate Diploma (Computer Science)**,&nbsp *Ashoka University*, India',
 '2019-2020')
-st.markdown('''
-- Minors in Computer Science, GPA: `3.6`
-- Relevant Courses - 
-\n Computer Sceince: &nbsp Advanced Machine Learning, Advanced Programing,  Algorithms, Data Mining and Warehousing
-\n Maths: &nbsp Linear Algebra, Calculus, Statistical Inference
-''')
+space, text = st.columns([0,8])
+with text:
+  st.markdown('''
+  -&nbspMinors in Computer Science, GPA: `3.6`
+  -&nbsp Deans List`
+  \n
+  -&nbspRelevant Courses : 
+  \n Computer Sceince: &nbsp Advanced Machine Learning, Advanced Programing,  Algorithms, Data Mining and Warehousing
+  \n Maths: &nbsp Linear Algebra, Calculus, Statistical Inference
+  ''', unsafe_allow_html = True)
 
-txt('**Bachelors of Arts** (Economics), *Ashoka University*, India',
+st.write("##")
+
+txt('**Bachelors of Arts (Economics)**,&nbsp *Ashoka University*, India',
 '2016-2019')
-st.markdown('''
-- Econmics Major, GPA: `3.3`
-\n
-- Relevant Courses : 
-\n &nbsp &nbsp <b>Economics:</b> &nbsp Advanced Econometrics, Statistics for Economics, Networks crowds and Internet, Game Theory
-\n &nbsp &nbsp <b>Finance:</b> &nbsp Advanced Financial Management, 
-\n &nbsp &nbsp <b>Computer Science:</b> &nbsp Introduction to Programing
+space, text = st.columns([0.5,8])
+with text:
+  st.markdown('''
+  -&nbspEconmics Major, GPA: `3.3`
+  \n
+  -&nbspRelevant Courses : 
+  \n &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Economics: &nbsp Advanced Econometrics, Statistics for Economics, Networks crowds and Internet, Game Theory
+  \n &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Finance: &nbsp Advanced Financial Management, 
+  \n &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Computer Science: &nbsp Introduction to Programing
 
-''', unsafe_allow_html = True)
+  ''', unsafe_allow_html = True)
 
 #####################
 st.markdown('''
 ## Work Experience
 ''')
 
-txt('**Head, Center of Data Mining and Biomedical Informatics**, Faculty of Medical Technology, Mahidol University, Thailand',
-'2011-2021')
-st.markdown('''
-- Managing a Center of `10` professors, researchers and students to ensure KPIs are strategically achieved namely to publish at least `20+` research publications annually. 
-- Actively took part in the talent hiring process as well as help employees to plan and develop their career path.
-- Set budget and handle procurement in order to facilitate education and research activities. Secured `> 10 million THB` budget.
-- Set and reflect on OKR on an annual basis to ensure productivity strategically matches the organization's direction.
-''')
+txt('**NLP Engineer**, Fractal Analytics',
+'Oct 2020-Present')
+space, text, space = st.columns([0.5,8,2])
+with text:
+  st.markdown('''
+  -Developed an intelligent automation tool that automates the processing of insurance claims (PA and FWA) for US health insurance firms. Solution digitizes and extracts relevant details from the insurance forms using Natural Language Processing (NLP) models/libraries like Spacy, PYSBD, and Regex, and applies validation rules to check if the claim is valid.
+  \n-Worked on developing an automated Invoice processing solution using Image OCR and Natural lan- guage processing. Trained a text classifier to identify relevant segments of an invoice. Used NLP to extract required information using pattern matching.
+  ''')
 
-txt('**Associate Professor**, Faculty of Medical Technology, Mahidol University, Thailand',
-'2012-2021')
-txt('**Assistant Professor**, Faculty of Medical Technology, Mahidol University, Thailand',
-'2009-2012')
-txt('**Lecturer**, Faculty of Medical Technology, Mahidol University, Thailand',
-'2006-2009')
-st.markdown('''
-- Provided mentorship and supervision to junior faculty, researchers, Ph.D./M.Sc./B.Sc. students. Mentored `3` Post-doctoral fellows, supervised `13` Ph.D. students, supervised `8` M.Sc. students, supervised `13` B.Sc. students and hosted `6` visiting students from U.S., Sweden and India.
-- Wrote and applied for research grants. Served as Principal Investigator for research grants that have been awarded `12.5 million THB` and `1.117 million SEK` in research funding from Thai and Swedish grant agencies.
-- Conducted research by applying machine learning to computational drug discovery and ensuring that research output exceeds `20+` articles per year.
-- Taught `10+` undergraduate/graduate classes on Bioinformatics, Data Mining, Scientific Research and Presentation, Research Methodology, Graduate Seminar, Programming for Health Data Science, etc.
-- Peer reviewed `100+` research articles for leading scientific journals.
-''')
+st.write("##")
+txt('**Research Assistant**, Dr. Ashwini Deshpande',
+'Aug - Sept 2021')
+space, text, space = st.columns([0.5,8,2])
+with text:
+  st.markdown('''
+  Worked with Dr. Ashwini Pande and Dr. Joshua Merfeld on the paper ‘Decentralization and Program Implementation’ to study the impact of district splits on the effectiveness of Program implementation. I prepared the dataset of over 800 districts and 7000 blocks using web scraping and census datasets. I identified district splits in India from 2011-20 by mapping block level changes in a district year over year, allowing us to study how district splits affected NREGA implementation in India from 2010-20.
+  ''')
+  
+st.write("##")
+txt('**Research Assistant**, Dr. Kanika Mahajan',
+'May - July 2020')
+space, text, space = st.columns([0.5,8,2])
+with text:
+  st.markdown('''
+  -Worked on the now published journal ‘Covid 19 and Supply chain disruption’. I was responsible for cleaning the data and calculating the distances between production zones and retail centres by using google maps API to find the impact of distance on supply chain disruptions.
+  \n-Worked on the paper ‘Words matter: Gender, Jobs and Applicant Behaviour’, to examine how implicit gender preference in Job ads leads to differential labor market out- comes. I used NLP techniques like tf-idf, topic modeling and word clouds to understand the difference between the kind of jobs offered and the keywords associated with respective gender preferred jobs.
+  ''')
 
-txt('**Co-Chair**, International Conference on Pharmaceutical Bioinformatics at Pattaya, Thailand',
-'2016')
-st.markdown('''
-- Oversee all aspects of the conference preparations from conception to launch. This include inviting keynote and plenary speakers, create advertisement flyers, create abstract book, etc.
-- Conference attracted `200+` participants from `40+` countries from university and industry sector.
-- Chaired keynote session, technical workshop and some of the parallel sessions.
-''')
+st.write("##")
+txt('**Business Analyst Intern**, National Skill Foundation of India',
+'June - July 2017')
+space, text, space = st.columns([0.5,8,2])
+with text:
+  st.markdown('''
+  Went to Jharkhand to study the Lac industry. Surveyed over 30 farmers and created a micro business plan for improving current NSFI’s college curriculum on growing LAC.
+  ''')
 
-txt('**Content Creator**, [Data Professor YouTube Channel](https://youtube.com/dataprofessor/)',
-'2019-Present')
-st.markdown('''
-- `100,000+` subscribers on YouTube
-- Created `261` educational videos on data science, machine learning and bioinformatics as well as hosted several podcast episodes with data scientists.
-- Created `3` sponsored videos for Notion, Gradio and Classpert.
-''')
+twitterazi_file = open('media/Twitterazi_demo_2.mp4', 'rb')
+twitterazi_bytes = twitterazi_file.read()
 
-txt('**Content Creator**, [Coding Professor YouTube Channel](https://youtube.com/codingprofessor/)',
-'2019-Present')
-st.markdown('''
-- `3,200+` subscribers on YouTube
-- Created `38` educational videos on Python and R programming.
-''')
+stocksnapshot_file = open('media/Stock_snapshot_demo.mp4', 'rb')
+stocksnapshot_bytes = stocksnapshot_file.read()
 
-txt('**Technical Writer**, [Data Professor Blog](https://data-professor.medium.com/) on Medium.com',
-'2019-Present')
-st.markdown('''
-- `4,100+` subscribers on Medium
-- Written `68` technical blogs on data science, machine learning and bioinformatics.
-''')
-
+bookspine_file = open('media/Book_spine_demo.mp4', 'rb')
+bookspine_bytes = bookspine_file.read()
 #####################
 st.markdown('''
-## Bioinformatics Tools
+## Projects
 ''')
-txt4('ABCpred', 'A web server for the discovery of acetyl- and butyryl-cholinesterase inhibitors', 'http://codes.bio/abcpred/')
-txt4('AutoWeka', 'An automated data mining software based on Weka', 'http://www.mt.mahidol.ac.th/autoweka/')
-txt4('ACPred', 'A computational tool for the prediction and analysis of anticancer peptides','http://codes.bio/acpred/')
-txt4('BioCurator', 'A web server for curating ChEMBL bioactivity data', 'http://codes.bio/biocurator/')
-txt4('CryoProtect', 'A web server for classifying antifreeze proteins from non-antifreeze proteins','http://codes.bio/cryoprotect/')
-txt4('ERpred', 'A web server for the prediction of subtype-specific estrogen receptor antagonists', 'http://codes.bio/erpred')
-txt4('HCVpred', 'A web server for predicting the bioactivity of Hepatitis C virus NS5B inhibitors', 'http://codes.bio/hemopred/')
-txt4('HemoPred', 'A web server for predicting the hemolytic activity of peptides', 'http://codes.bio/hemopred/')
-txt4('iQSP', 'A sequence-based tool for the prediction and analysis of quorum sensing peptides', 'http://codes.bio/iqsp/')
-txt4('Meta-iAVP', 'A sequence-based meta-predictor for improving the prediction of antiviral peptides', 'http://codes.bio/meta-iavp/')
-txt4('osFP', 'A web server for predicting the oligomeric state of fluorescent proteins', 'http://codes.bio/osfp/')
-txt4('PAAP', 'A web server for predicting antihypertensive activity of peptides', 'http://codes.bio/paap/')
-txt4('PepBio', 'A web server for predicting the bioactivity of host defense peptide', 'http://codes.bio/pepbio')
-txt4('PyBact', 'Open source software written in Python for bacterial identification', 'https://sourceforge.net/projects/pybact/')
-txt4('TargetAntiAngio', 'A sequence-based tool for the prediction and analysis of anti-angiogenic peptides','http://codes.bio/targetantiangio/')
-txt4('ThalPred', 'Development of decision model for discriminating Thalassemia trait and Iron deficiency anemia','http://codes.bio/thalpred/')
-txt4('THPep', 'A web server for predicting tumor homing peptides','http://codes.bio/thpep/')
+txt4('**Book Spine Detection**', 'Developed an app for a client that segregates and digitizes the books in a book shelf\'s image. Uses canny edge detection method to detect edges in a photo and then applies hough transformation to identify book spine edges. Then the image is cropped on the identified edges and digitized to get the text on spine.', 'none')
+space, exp, space = st.columns([1,3.8,1.1])
+with exp:
+  with st.expander("See Demo"):
+    st.video(bookspine_bytes)
 
+txt4('**Twitterazi**', 'A web app that finds the topics a twitter user has tweeted about in the past month. It accurately recognises people, organisations, locations and keywords in the tweets using Spacy and Regex, which can then be used to filter out tweets containing that entity.', '[Link](https://sanchitgoel7-twitterazi-app-ntuijm.streamlitapp.com/)')
+space, exp, space = st.columns([1,3.8,1.1])
+with exp:
+  with st.expander("See Demo"):
+    st.video(twitterazi_bytes)
+
+txt4('**Stocksnapshot**', 'Developed a one-stop stock analysis tool that gives a financial overview of a company by scraping over 20yr+ financial data. The App also has a built in DCF calculator for a quick Intrinsic value calculation of a stock.', '[Link](https://stocksnapshot.herokuapp.com/) &nbsp &nbsp [Github](https://stocksnapshot.herokuapp.com/)')
+
+space, vid, space = st.columns([1,3.8,1.1])
+with vid:
+  with st.expander("See Demo"):
+    st.video(stocksnapshot_bytes)
 
 #####################
 st.markdown('''
 ## Skills
 ''')
-txt3('Programming', '`Python`, `R`, `Linux`')
-txt3('Data processing/wrangling', '`SQL`, `pandas`, `numpy`')
-txt3('Data visualization', '`matplotlib`, `seaborn`, `plotly`, `altair`, `ggplot2`')
+txt3('Programming', '`Python`, `SQL`,`Java`')
+txt3('Natural Language Processing','`Spacy`,`NLTK`,`PYSBD`,`TF-DF`,`LDA`')
+txt3('Data visualization', '`PowerBi`,`Tableau`,`matplotlib`, `seaborn`, `altair`')
 txt3('Machine Learning', '`scikit-learn`')
 txt3('Deep Learning', '`TensorFlow`')
-txt3('Web development', '`Flask`, `HTML`, `CSS`')
-txt3('Model deployment', '`streamlit`, `gradio`, `R Shiny`, `Heroku`, `AWS`, `Digital Ocean`')
+txt3('Web development', '`streamlit`, `CSS`')
+txt3('Model deployment', '`streamlit`,`Heroku`, `AWS`')
 
 #####################
 st.markdown('''
@@ -228,10 +231,4 @@ st.markdown('''
 txt2('LinkedIn', 'https://www.linkedin.com/in/chanin-nantasenamat')
 txt2('Twitter', 'https://twitter.com/thedataprof')
 txt2('GitHub', 'https://github.com/chaninn/')
-txt2('', 'https://github.com/chaninlab/')
-txt2('', 'https://github.com/dataprofessor')
-txt2('ORCID', 'http://orcid.org/0000-0003-1040-663X')
-txt2('Scopus', 'http://www.scopus.com/authid/detail.url?authorId=12039071300')
-txt2('ResearcherID', 'http://www.researcherid.com/rid/F-1021-2010')
-txt2('ResearchGate', 'https://www.researchgate.net/profile/Chanin_Nantasenamat')
-txt2('Publons', 'https://publons.com/a/303133/')
+
